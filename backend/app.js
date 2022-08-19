2d803d0831b7f996a1ea6f4144cbaf2bdece334e
ohsand <osand.business@gmail.com>
2022-08-19 15:26:35 +0200

    add token

--- backend/app.js
+++ backend/app.js
@@ -5,21 +5,23 @@ mongoose.connect('mongodb+srv://ohsand:12345@cluster0.uonbuyb.mongodb.net/?retry
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-//const sauces = require('./models/sauces');
+const sauces = require('./models/sauces');
 
 const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
-  const app = express();
+const app = express();
 
-  app.use(express.json());
+app.use(express.json());
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  app.use('/api/stuff', saucesRoutes);
+app.use('/api/stuff', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true }
-})
+});
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
