dbc1b32032b9bb6efce33bfebbbef531d24c380a
ohsand <osand.business@gmail.com>
2022-08-25 10:59:34 +0200

    middleware

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const sauces = require('../models/sauces');
+//const sauces = require('../models/sauces');
 const saucesCtrl = require('../controllers/stuff');
+const auth = require('auth');
+const multer = require('../middleware/multer-config.js');
 
-router.post('/', saucesCtrl.createSauces);
-router.put('/:id', saucesCtrl.modifySauces);
-router.delete('/:id', saucesCtrl.deleteSauces);
-router.get('/:id', saucesCtrl.getOneSauces);
-router.get('/', saucesCtrl.getAllSauces);
+router.post('/', auth, multer, saucesCtrl.createSauces);
+router.put('/:id', auth, saucesCtrl.modifySauces);
+router.delete('/:id', auth, saucesCtrl.deleteSauces);
+router.get('/:id', auth, saucesCtrl.getOneSauces);
+router.get('/', auth, saucesCtrl.getAllSauces);
 
 module.exports = router;
