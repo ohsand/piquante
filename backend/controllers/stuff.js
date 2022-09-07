5bcf12569eb359664fdb733b7eeba16020627fcc
ohsand <osand.business@gmail.com>
2022-09-07 15:23:45 +0200

    remove console log

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,7 +1,9 @@
-const { textChangeRangeIsUnchanged } = require('typescript');
+//const { textChangeRangeIsUnchanged } = require('typescript');
 const Sauce = require('../models/sauce');
 const fs = require('fs');
 
+
+// creaate a new sauce
 exports.createSauces = (req, res, next) => {
     console.log('enter here ');
     console.log('req 1.1 ? ' + JSON.stringify(req.body));
@@ -14,33 +16,34 @@ exports.createSauces = (req, res, next) => {
     delete saucesObject._id;
     delete saucesObject._userId;
     console.log('req ath ' + req.auth.userId);
-    const sauces = new Sauce({
+    const sauce = new Sauce({
       ...saucesObject,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
 
-    sauces.save()
+    sauce.save()
     .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
   };
 
+// edit an existing sauce
 exports.modifySauces = (req, res, next) => {
   /*sauces.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
     .catch(error => res.status(400).json({ error }));*/
-    const saucesObject = req.file ? {
+    const sauceObject = req.file ? {
       ...JSON.parse(req.body.sauces),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : {...req.body};
 
-    delete saucesObject.userId;
+    delete sauceObject.userId;
     Sauce.findOne({_id: req.params.id})
-    .then((sauces) => {
-      if (sauces.userId != req.auth.userId) {
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
         res.status(400).json({ message : "non autorisé" });
       } else {
-        sauces.updateOne({ _id: req.params.id}, { ...saucesObject, _id: req.params.id})
+        sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
         .then(() => res.status(200).json({message : 'Objet mofidifié' }))
         .catch(error => res.status(400).json({ error }));
       }
@@ -50,6 +53,7 @@ exports.modifySauces = (req, res, next) => {
     })
   };
 
+//delete a sauce
 exports.deleteSauces = (req, res, next) => {
     /*sauces.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
@@ -72,13 +76,21 @@ exports.deleteSauces = (req, res, next) => {
     });
   };
 
-exports.getOneSauces = (req, res, next) => {
+/*exports.getOneSauces = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauces) => res.status(200).json(sauces))
     console.log(sauces)
     .catch(error => res.status(404).json({ error }));
+  };*/
+
+//get one sauce
+exports.getOneSauces = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauces) => res.status(200).json(sauces))
+    .catch(error => res.status(404).json({ error }));
   };
 
+//get all sauces
 exports.getAllSauces = (req, res, next) => {
   console.log("finding sauces");
   Sauce.find()
